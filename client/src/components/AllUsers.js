e0809bd21f0d8a3e1f48842deb55693accc48cfb
Daniel Ghazvinie <danielghazvinie@protonmail.com>
2021-11-03 17:57:48 +0000

    setting up users page

--- client/src/components/AllUsers.js
+++ client/src/components/AllUsers.js
@@ -1,8 +1,28 @@
-export default function AllUsers (){
+export default function AllUsers({ usersData }) {
+
+    console.log(usersData)
+
+    const usersTable = usersData.map(user => (
+        <div className='user' key={user.id.value}>
+
+                <img src={user.picture.thumbnail} alt={`${user.name.first} ${user.name.last}`}/>
+
+                <p>{user.name.title}. {`${user.name.first} ${user.name.last}`}</p>
+
+
+                <p>{user.login.username}</p>
+
+                <p>{user.email}</p>
+
+        </div>
+    ))
+
+
     return (
         <div>
-            All Users
+            {usersTable}
         </div>
     );
 
 }
+
