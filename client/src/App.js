bedd76b3ecea25f1247ed1c32ffbf24564e52966
Daniel Ghazvinie <danielghazvinie@protonmail.com>
2021-11-03 17:30:44 +0000

    setup fetching data

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,23 @@
-
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [usersData, setUsersData] = useState([]);
+
+
+  useEffect(() => { // Fetch array of users
+    const getData = async () => {
+      const res = await fetch('https://randomuser.me/api/?results=20');
+      const { results } = await res.json();
+      setUsersData(results)
+    };
+    getData();
+  }, []);
+
+
   return (
     <div>
-Hello
+      Hello
     </div>
   );
 }
