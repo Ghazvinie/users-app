0b0b795cf8b9886b1e9b18854844f9b06940f928
Daniel Ghazvinie <danielghazvinie@protonmail.com>
2021-11-03 20:39:00 +0000

    adding some styling to single user

--- client/src/components/UserDetail.js
+++ client/src/components/UserDetail.js
@@ -1,18 +1,21 @@
 import React from "react";
+import '../styles/singleuser.css'
 
-export default function UserDetail({user}) {
+export default function UserDetail({ user }) {
     return (
-        <div>
-        <img src={user.picture.large} alt={`${user.name.first} ${user.name.last}`} />
+        <div className='single-container'>
+            <img src={user.picture.large} alt={`${user.name.first} ${user.name.last}`} />
 
-        <p>{user.name.title}. {`${user.name.first} ${user.name.last}`}</p>
+            <div className='user-details'>
+                <p className='title'>Full Name: {user.name.title}. {`${user.name.first} ${user.name.last}`}</p>
 
-        <p>{user.login.username}</p>
+                <p>Username: {user.login.username}</p>
 
-        <p>{user.location.country}</p>
+                <p>Country: {user.location.country}</p>
 
-        <p>{user.email}</p>
+                <p>Email: {user.email}</p>
+            </div>
 
-    </div>
+        </div>
     )
 }
